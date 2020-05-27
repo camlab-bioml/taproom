25c27190284efd25ba3c65dc77c28b4990a1a274
michael-geuenich <mgeuenic@galen.mshri.on.ca>
2020-05-27 11:56:55 -0700

    create colour palette functions

---
+++ R/colours.R
@@ -0,0 +1,43 @@
+jackson_basel_colours <- function() {
+  pal <- ggthemes::tableau_color_pal(palette = "Classic Green-Orange 12")(12)
+  
+  celltype_colours <- c(
+    "Stromal" = pal[7],
+    "B cells" = pal[3],
+    "T cells" = pal[8],
+    "Macrophage" = pal[1],
+    "Epithelial (basal)" = pal[9],
+    "Epithelial (luminal)" = pal[10],
+    "Epithelial (other)" = pal[5],
+    "Monocyte" = pal[11],
+    "Other" = "grey60",
+    "Unknown" = "grey70"
+  )
+  celltype_colours
+}
+
+
+patient_clinSubtype_colours <- function(){
+  pal <- c("#648FFF", "#785EF0", "#DC267F", "#FE6100", "#FFB000")
+  
+  clinSubtype_colours <- c(
+    "HR+HER2-" = pal[1],
+    "TripleNeg" = pal[3],
+    "HR+HER2+" = pal[4],
+    "HR-HER2+" = pal[5],
+    "NA" = "lightgrey"
+  )
+  clinSubtype_colours
+}
+
+patient_response_colours <- function(){
+  pal <- c("#648FFF", "#785EF0", "#DC267F", "#FE6100", "#FFB000")
+  
+  clinSubtype_colours <- c(
+    "Resistant" = pal[3],
+    "Refractory" = pal[4],
+    "Sensitive" = pal[1],
+    "NA" = "lightgrey"
+  )
+  clinSubtype_colours
+}
