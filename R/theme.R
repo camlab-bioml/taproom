92f95bf7dce0539182b2752e57a1c2d19630925f
Michael-Geuenich <michaelgeuenich@yahoo.com>
2020-07-29 14:38:06 -0400

    minor astir theme edits

--- R/theme.R
+++ R/theme.R
@@ -3,5 +3,7 @@
 astir_paper_theme <- function() {
   cowplot::theme_cowplot(font_size = 12) +
     theme(strip.background = element_rect(fill = "white"),
-          strip.text = element_text(face="bold"))
+          strip.text = element_text(face="bold"),
+          legend.text=element_text(size=12),
+          legend.title=element_text(face = "bold", size=14))
 }
